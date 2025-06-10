1881f75975c13e417ced35e7cb7c74c308e7e2f5
C0A24188 <c0a24188b5@edu.teu.ac.jp>
2025-06-10 15:47:29 +0900

    反転背景画像の描画

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #左辺は背景画像を表すsurface
+    bg_reverse_img = pg.transform.flip(bg_img,True,False)
     tmr = 0
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
@@ -19,7 +20,7 @@ def main():
 
         x = tmr
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img,[1600-x,0])
+        screen.blit(bg_reverse_img,[1600-x,0])
         screen.blit(kk_img,[30,200])
         pg.display.update()
         tmr += 1        
